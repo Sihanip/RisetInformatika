8319288936649809f090805c10b1b26a4170852f
Sihanip <hanifkadot@gmail.com>
2023-10-02 01:17:26 +0700

    Create Tugas 1

---
+++ Tugas1.js
@@ -0,0 +1,63 @@
+
+    ===== TUGAS 1 : Mencari Topik Penelitian =====
+
+Nama        : Hanif Nur Fadlilah 
+NPM         : 20081010231 
+Kelas       : D081 
+Mata Kuliah : Riset Informatika 
+
+
+=== TOPIK PENELITIAN ===
+_ Pengembangan Website Manajemen Stok Makanan Berbasis Barcode _
+
+=== PERSOALAN PRAKTIS ===
+Pengembangan Website Manajemen Stok Makanan Berbasis Barcode dapat membantu mengatasi berbagai persoalan praktis dalam industri makanan. 
+Berikut adalah beberapa persoalan praktis yang dapat diselesaikan melalui penelitian dan pengembangan semacam ini:
+1. Ketidakefisienan Manajemen Stok: 
+    Banyak bisnis makanan, seperti restoran atau supermarket, sering mengalami kesulitan dalam mengelola stok mereka dengan efisien. 
+    Sistem barcode dapat membantu dalam mengotomatisasi proses pelacakan stok dan mengurangi risiko overstock atau understock.
+2. Kadaluwarsa Makanan: 
+    Salah satu persoalan besar dalam industri makanan adalah meminimalkan pemborosan makanan yang kadaluwarsa. Dengan sistem barcode yang tepat, 
+    bisnis dapat dengan mudah melacak tanggal kedaluwarsa dan mengambil tindakan yang diperlukan untuk mengurangi pemborosan.
+3. Kesalahan Pencatatan Manual: 
+    Pencatatan stok makanan secara manual sering kali rentan terhadap kesalahan manusiawi. Sistem barcode dapat mengurangi 
+    atau bahkan menghilangkan kesalahan pencatatan manual, yang dapat menyebabkan ketidakcocokan antara data stok yang ada dan yang sebenarnya.
+4. Pelacakan Pemakaian Bahan Baku: 
+    Dalam industri makanan, penting untuk melacak pemakaian bahan baku yang akurat. 
+    Sistem barcode dapat digunakan untuk mencatat pemakaian bahan baku secara real-time, sehingga bisnis dapat mengoptimalkan pembelian dan produksi.
+5. Integrasi dengan Point of Sale (POS): 
+    Integrasi antara sistem manajemen stok berbasis barcode dengan POS dapat membantu dalam mengotomatisasi proses pelacakan 
+    penjualan dan pengurangan stok secara real-time. Ini dapat menghindari kesalahan akibat perbedaan antara penjualan dan stok aktual.
+6. Analisis Kinerja: 
+    Sistem ini dapat memberikan data yang berguna untuk menganalisis kinerja bisnis, 
+    seperti penjualan terbaik, item yang kurang laku, dan tren permintaan. Ini dapat membantu pemilik bisnis dalam membuat keputusan yang lebih baik.
+7. Pengurangan Biaya Tenaga Kerja: 
+    Dengan otomatisasi manajemen stok, bisnis dapat mengurangi keterlibatan tenaga kerja dalam proses pelacakan stok, 
+    yang pada gilirannya dapat mengurangi biaya operasional.
+8. Peningkatan Efisiensi dan Layanan Pelanggan: 
+    Dengan manajemen stok yang lebih baik, bisnis dapat meningkatkan efisiensi operasional mereka, 
+    sehingga dapat melayani pelanggan dengan lebih baik dengan menghindari ketidaktersediaan produk yang diinginkan.
+
+=== RESEARCH QUESTIONS (PERTANYAAN PENELITIAN) ===
+Berdasarkan persoalan praktis di atas, maka dapat dirumuskan beberapa pertanyaan penelitian, yakni:
+1. Bagaimana implementasi sistem barcode dalam manajemen stok makanan dapat meningkatkan efisiensi operasional bisnis makanan?
+2. Apakah integrasi antara sistem manajemen stok berbasis barcode dengan Point of Sale (POS) memiliki dampak positif 
+    pada akurasi pelacakan penjualan dan pengurangan stok secara real-time?
+3. Sejauh mana penggunaan sistem manajemen stok berbasis barcode dapat mengurangi biaya tenaga kerja dalam industri makanan?
+4. Bagaimana implementasi sistem barcode dalam manajemen stok makanan dapat meningkatkan layanan pelanggan dalam industri makanan?
+5. Apakah ada faktor-faktor tertentu yang mempengaruhi adopsi dan penerimaan sistem manajemen stok berbasis barcode dalam bisnis makanan?
+6. Apa tantangan dan hambatan yang mungkin dihadapi dalam implementasi sistem barcode dalam manajemen stok makanan, dan bagaimana cara mengatasinya?
+
+=== TEORI TERKAIT ===
+Dalam penelitian ini merujuk pada beberapa teori yang relevan, yakni:
+1. Teori Pengembangan Aplikasi Web (Web Application Development Theory): 
+    Dalam hal pengembangan website, Anda dapat mempelajari teori dan praktik 
+    terkini dalam pengembangan aplikasi web, termasuk arsitektur, keamanan, dan antarmuka pengguna yang baik.
+2. Teori Sistem Informasi Geografis (SIG/GIS): 
+    Jika ada elemen lokasi dalam manajemen stok makanan (misalnya, rantai pasokan dengan lokasi gudang yang berbeda), 
+    teori SIG dan GIS dapat digunakan untuk memahami bagaimana informasi geografis dapat diintegrasikan dalam 
+    sistem barcode untuk manajemen stok yang lebih baik.
+3. Teori Manajemen Persediaan: 
+    Teori ini mencakup berbagai model dan metode untuk mengelola persediaan makanan, 
+    Penelitian dapat menggali bagaimana penggunaan sistem barcode dapat memengaruhi dan diterapkan dalam manajemen persediaan.
+
